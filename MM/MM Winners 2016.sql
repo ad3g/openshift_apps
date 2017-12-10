479b78d23ebd8f5fa484ad9bf34de3f891a6ecb5
ad3g <31421802+ad3g@users.noreply.github.com>
2017-12-10 11:17:41 -0500

    Add files via upload

---
+++ MM/MM Winners 2016.sql
@@ -0,0 +1,46 @@
+/*
+-- Query: SELECT * FROM apps.mm_winner
+-- Date: 2017-12-10 11:17
+*/
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('1688761','final','2016-03-18','http:\\\\www.ncaa.com/game/basketball-men/d1/2016/03/18/bakersfield-oklahoma','Hoy',30);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('1688761','half','2016-03-18','http:\\\\www.ncaa.com/game/basketball-men/d1/2016/03/18/bakersfield-oklahoma','Annette',15);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('1688764','final','2016-03-18','http:\\\\www.ncaa.com/game/basketball-men/d1/2016/03/18/stephen-f-austin-west-virginia','Cindy M.',30);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('1688764','half','2016-03-18','http:\\\\www.ncaa.com/game/basketball-men/d1/2016/03/18/stephen-f-austin-west-virginia','Rico',15);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('1688773','final','2016-03-18','http:\\\\www.ncaa.com/game/basketball-men/d1/2016/03/18/hawaii-california','Jeremy',30);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('1688773','half','2016-03-18','http:\\\\www.ncaa.com/game/basketball-men/d1/2016/03/18/hawaii-california','Ciglo',15);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('1688779','final','2016-03-18','http:\\\\www.ncaa.com/game/basketball-men/d1/2016/03/18/cincinnati-saint-josephs','Wiemer',30);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('1688779','half','2016-03-18','http:\\\\www.ncaa.com/game/basketball-men/d1/2016/03/18/cincinnati-saint-josephs','Colligan II',15);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('1688782','final','2016-03-18','http:\\\\www.ncaa.com/game/basketball-men/d1/2016/03/18/holy-cross-oregon','Corrigan',30);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('1688782','half','2016-03-18','http:\\\\www.ncaa.com/game/basketball-men/d1/2016/03/18/holy-cross-oregon','Farts',15);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('1688784','final','2016-03-18','http:\\\\www.ncaa.com/game/basketball-men/d1/2016/03/18/syracuse-dayton','Curtis',30);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('1688784','half','2016-03-18','http:\\\\www.ncaa.com/game/basketball-men/d1/2016/03/18/syracuse-dayton','Farts',15);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('1688786','final','2016-03-18','http:\\\\www.ncaa.com/game/basketball-men/d1/2016/03/18/pittsburgh-wisconsin','Jimmer / Buda',30);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('1688786','half','2016-03-18','http:\\\\www.ncaa.com/game/basketball-men/d1/2016/03/18/pittsburgh-wisconsin','Farts',15);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('1688791','final','2016-03-18','http:\\\\www.ncaa.com/game/basketball-men/d1/2016/03/18/south-dakota-st-maryland','Rolli',30);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('1688791','half','2016-03-18','http:\\\\www.ncaa.com/game/basketball-men/d1/2016/03/18/south-dakota-st-maryland','John Lee',15);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('1688792','final','2016-03-18','http:\\\\www.ncaa.com/game/basketball-men/d1/2016/03/18/unc-asheville-villanova','Farts',30);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('1688792','half','2016-03-18','http:\\\\www.ncaa.com/game/basketball-men/d1/2016/03/18/unc-asheville-villanova','Cindy M.',15);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('1688801','final','2016-03-18','http:\\\\www.ncaa.com/game/basketball-men/d1/2016/03/18/temple-iowa','Kaplan',30);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('1688801','half','2016-03-18','http:\\\\www.ncaa.com/game/basketball-men/d1/2016/03/18/temple-iowa','Forbush',15);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('1688802','final','2016-03-18','http:\\\\www.ncaa.com/game/basketball-men/d1/2016/03/18/weber-st-xavier','Osmond',30);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('1688802','half','2016-03-18','http:\\\\www.ncaa.com/game/basketball-men/d1/2016/03/18/weber-st-xavier','Dunny',15);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('1688806','final','2016-03-18','http:\\\\www.ncaa.com/game/basketball-men/d1/2016/03/18/vcu-oregon-st','Shoemaker',30);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('1688806','half','2016-03-18','http:\\\\www.ncaa.com/game/basketball-men/d1/2016/03/18/vcu-oregon-st','Batch',15);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('1688811','final','2016-03-18','http:\\\\www.ncaa.com/game/basketball-men/d1/2016/03/18/middle-tenn-michigan-st','Curtis',30);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('1688811','half','2016-03-18','http:\\\\www.ncaa.com/game/basketball-men/d1/2016/03/18/middle-tenn-michigan-st','Cheryl',15);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('1688822','final','2016-03-18','http:\\\\www.ncaa.com/game/basketball-men/d1/2016/03/18/uni-texas','Leo',30);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('1688822','half','2016-03-18','http:\\\\www.ncaa.com/game/basketball-men/d1/2016/03/18/uni-texas','Buda / JG',15);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('1688823','final','2016-03-18','http:\\\\www.ncaa.com/game/basketball-men/d1/2016/03/18/green-bay-texas-am','DAK / TIFF',30);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('1688823','half','2016-03-18','http:\\\\www.ncaa.com/game/basketball-men/d1/2016/03/18/green-bay-texas-am','Osmond',15);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('1688826','final','2016-03-18','http:\\\\www.ncaa.com/game/basketball-men/d1/2016/03/18/michigan-notre-dame','BNB',30);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('1688826','half','2016-03-18','http:\\\\www.ncaa.com/game/basketball-men/d1/2016/03/18/michigan-notre-dame','Holmsy',15);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('Match00','final','loaddate','loadurl','Smiley Bill',50);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('Match11','final','loaddate','loadurl','T. Hampton',50);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('Match22','final','loaddate','loadurl','Ruffer',50);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('Match33','final','loaddate','loadurl','Stickman',50);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('Match44','final','loaddate','loadurl','CZECH',50);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('Match55','final','loaddate','loadurl','Hufton',50);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('Match66','final','loaddate','loadurl','Farts',50);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('Match77','final','loaddate','loadurl','Matt M.',50);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('Match88','final','loaddate','loadurl','Jimmer / Roger',50);
+INSERT INTO `mm_winner` (`GAMEID`,`PERIOD`,`STDATE`,`URL`,`PLAYER`,`AMT`) VALUES ('Match99','final','loaddate','loadurl','Rod',50);
